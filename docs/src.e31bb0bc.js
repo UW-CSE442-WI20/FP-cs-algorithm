4bda1c583942566791015e7a2074fcd146fded67
ruthviksai <ruthvs@uw.edu>
2020-03-02 23:35:50 -0800

    rebuilt

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -29425,7 +29425,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63060" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63271" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
